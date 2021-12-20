1586d10202b02c9346a8c5e90a850b9c3ceacdd8
SoftwareSmeden <torb815g@edu.easj.dk>
2021-12-20 19:40:22 +0100

    1.6

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module tat.trackandtrace {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
 
 
     opens Domaene.Controller to javafx.fxml;
